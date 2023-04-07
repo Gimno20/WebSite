e2358380d5bee569dffb9093b98093ef36e82fa5
Gimno20 <99756501+Gimno20@users.noreply.github.com>
2023-04-07 10:11:37 +0400

    commit BackEnd firstCom

---
+++ laravel-api/app/Http/Resources/ClientsResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'created_at' => $this->created_at,
+            'lists' => $this->lists
+        ];
+    }
+}
